270e965a38fba8efc84528ee0d295e424be90643
seanaleid <seanaleid@gmail.com>
2019-11-08 11:37:48 -0500

    Adds index.js and server.js files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// import projects router here
+// import resources router here
+// import tasks router here
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
+module.exports = server;
