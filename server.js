76065d5875d98b06b459632194db4f986b3f70e8
seanaleid <seanaleid@gmail.com>
2019-11-08 11:42:15 -0500

    Adds knexfile.js and dbConfig.js files

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
-server.use('/api/projects', projectsRouter);
-server.use('/api/resources', resourcesRouter);
-server.use('/api/tasks', tasksRouter);
+// server.use('/api/projects', projectsRouter);
+// server.use('/api/resources', resourcesRouter);
+// server.use('/api/tasks', tasksRouter);
 
 module.exports = server;
